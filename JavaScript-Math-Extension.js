f745c5a896fdc06408c0b2f16cd6cbf1775050d8
Fantaland <Fantaland@outlook.com>
2016-03-04 15:16:39 -0700

    Update JavaScript-Math-Extension.js

--- JavaScript-Math-Extension.js
+++ JavaScript-Math-Extension.js
@@ -97,8 +97,12 @@ Math.lawCosAngle = function(a, b, c)
 }
 
 //CIRCLES SECTION
+Math.circle = {
+
+}
+
 //Find an arc in a circle.  Note this is an untested function.
-Math.findArc = function(c, r) 
+Math.circle.findArc = function(c, r) 
 {
 	//c is the angle in degrees
 	//r is radius
@@ -108,41 +112,41 @@ Math.findArc = function(c, r)
 }
 
 //Finds area of circle
-Math.findAreaCircle = function(r) 
+Math.circle.findArea = function(r) 
 {
 	//r is radius
 	return Math.PI * (r * r);
 }
 
 //Finds the radius of circle with the area
-Math.findRadiusCircleArea = function(a)
+Math.circle.findRadiusByArea = function(a)
 {
 	//a is area
 	return Math.sqrt(a / Math.PI);
 }
 
 //Find the radius of the circle with the circumference
-Math.findRadiusCircleCircum = function(c)
+Math.circle.findRadiusByCircum = function(c)
 {
 	//c is circumference
 	return c / 2 / Math.PI;
 }
 
 //Finds diameter of circle
-Math.findDiameterCircle = function(r) 
+Math.circle.findDiameter = function(r) 
 {
 	//r is radius
 	return n * 2;
 }
 
-Math.findCircumference = function(r) 
+Math.circle.findCircumference = function(r) 
 {
 	//r is radius
 	return 2 * Math.PI * r;
 }
 
 //Finds a missing angle within a circle
-Math.findCircleMissingCentralAngle = function()
+Math.circle.findMissingCentralAngle = function()
 {
 	var total;
 	
